7550495694f7e682e863a3379ef50b45e5f563f5
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2022-07-24 11:02:23 +1000

    update jpghdr to C89, unified makefile

--- jpeghdr.c
+++ jpeghdr.c
@@ -4,22 +4,22 @@
 #include "log.h"
 
 int main(int argc, char *argv[]) {
+	FILE *f;
+	int hdr_sz;
+
 	if (argc < 2) {
 		printf("jpeghdr prints the header size of the JPEG file\n");
 		printf("usage: %s <file.jpg>\n", argv[0]);
 		return 1;
 	}
-
-	FILE *f = fopen(argv[1], "rb");
-	if (f == NULL) {
+	
+	if ((f = fopen(argv[1], "rb")) == NULL) {
 		perror("open error");
 		return 1;
 	}
 	set_log_level(LOG_LEVEL_TRACE);
-
-	int hdr_sz = jpeg_hdr_size(f);
-
-	if (!hdr_sz) {
+	
+	if (!(hdr_sz = jpeg_hdr_size(f))) {
 		fclose(f);
 		fprintf(stderr, "there were errors\n");
 		return 1;
